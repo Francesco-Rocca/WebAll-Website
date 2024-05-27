3a9a6890ed70094bb670edc1f69506f7116005a0
Alessandro-Salerno <OxyGen05@outlook.it>
2024-05-27 14:29:30 +0200

    companies

---
+++ backend/reg_aziende.php
@@ -0,0 +1,46 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "weball";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Customers (c_type, email, password_hash, billing_address, registration_date, phone) VALUES ('I', ?, ?, ?, CURDATE(), ?)";
+$stm = $conn->prepare($sql);
+
+if (!isset($_POST["ragionsoc"])
+      || !isset($_POST["indirizzo-sede"])
+      || !isset($_POST["num-tel"])
+      || !isset($_POST["email"])
+      || !isset($_POST["password"])
+      || !isset($_POST["password-conf"])
+      || !isset($_POST["indirizzo"])) {
+
+    header("location: ../registrazione/utenti.php");
+}
+
+$ragione_sociale = $_POST["ragionsoc"];
+$sede = $_POST["indirizzo-sede"];
+$numtel = $_POST["num-tel"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$password_conf = $_POST["password-conf"];
+$indirizzo = $_POST["indirizzo"];
+
+
+$stm->bind_param("ssss", $email, $password, $indirizzo, $numtel);
+$s = $stm->execute();
+
+$sql = "INSERT INTO Organizations (id_customer, name, hq_address) VALUES (LAST_INSERT_ID(), ?, ?)";
+$stm = $conn->prepare($sql);
+
+$stm->bind_param("ss", $ragione_sociale, $sede);
+$s = $stm->execute();
--- backend/reg_utenti.php
+++ backend/reg_utenti.php
@@ -1,23 +1,31 @@
  <?php
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
+
 $servername = "127.0.0.1";
 $username = "root";
 $password = "root";
 $dbname = "weball";
 
-print_r($_POST);
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "<br>";
-print_r($conn->query("SELECT * FROM Customers")->fetch_assoc());
 
 $sql = "INSERT INTO Customers (c_type, email, password_hash, billing_address, registration_date, phone) VALUES ('I', ?, ?, ?, CURDATE(), ?)";
 $stm = $conn->prepare($sql);
+
+if (!isset($_POST["nome"])
+      || !isset($_POST["cognome"])
+      || !isset($_POST["num-tel"])
+      || !isset($_POST["email"])
+      || !isset($_POST["password"])
+      || !isset($_POST["password-conf"])
+      || !isset($_POST["indirizzo"])) {
+
+    header("location: ../registrazione/utenti.php");
+}
 
 $nome = $_POST["nome"];
 $cognome = $_POST["cognome"];
@@ -30,17 +38,9 @@ $indirizzo = $_POST["indirizzo"];
 
 $stm->bind_param("ssss", $email, $password, $indirizzo, $numtel);
 $s = $stm->execute();
-if (!$s) {
-    die($stm->error);
-}
 
 $sql = "INSERT INTO Individuals (id_customer, first_name, last_name) VALUES (LAST_INSERT_ID(), ?, ?)";
 $stm = $conn->prepare($sql);
 
 $stm->bind_param("ss", $nome, $cognome);
 $s = $stm->execute();
-if (!$s) {
-    die($stm->error);
-}
-$conn->close();
-?> 
